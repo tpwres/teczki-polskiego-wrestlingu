f7ee1f31d8380b264008fd15069797f6f79f7cda
Krzysztof Zych <k33rni@gmail.com>
2025-05-10 16:16:44 +0200

    Switch notation - graph author now decides on bar split, and can stack them.
    No more auto layout. Additionally, a special layer is added for vlines marking dates.

--- src/bin/membership.py
+++ src/bin/membership.py
@@ -2,7 +2,9 @@ import sys
 from pathlib import Path
 srcdir = Path(__file__).resolve().parent / '..'
 sys.path.insert(0, str(srcdir))
+import re
 import argparse
+from typing import ClassVar, Any
 from sys import stdin, stdout
 from types import SimpleNamespace
 from dateutil.rrule import YEARLY, MONTHLY
@@ -21,7 +23,8 @@ from collections import defaultdict
 import math
 
 def setup():
-    pass
+    Stripe.org_colors = load_org_colors()
+    plt.rcParams["hatch.linewidth"] = 6
 
 def load_org_colors():
     config_path = Path(__file__).parent / '../..' / 'config.toml'
@@ -37,17 +40,30 @@ def ym(text: str) -> datetime:
 def ymd(text: str) -> datetime:
     return datetime.strptime('2099-12-31' if text == '-' else text, '%Y-%m-%d')
 
-class CareerRow:
+class Stripe:
+    org_colors: ClassVar[dict[str, str]]
+    fallback_color: ClassVar[str] = '#000'
+
     def __init__(self, row):
-        name, org, start, end = row
-        self.name = name
-        self.org = org
-        self.start = ym(start)
-        self.end = ym(end)
+        name, org, start, end, *rest = row
+        self.name, self.org = name, org
+        self.start = ymd(start) if start.count('-') == 2 else ym(start)
+        self.end = ymd(end) if end.count('-') == 2 else ym(end)
         if end == '-':
             self.duration = datetime.today() - self.start
         else:
             self.duration = self.end - self.start
+        match rest:
+            case [layer, band]:
+                self.layer = layer
+                self.band = tuple(int(num) for num in band.split('/'))
+            case [layer]:
+                self.layer = layer
+                self.band = (1, 1)
+            case []:
+                self.layer = '0'
+                self.band = (1, 1)
+
 
     def overlaps(self, other_row):
         if self.start >= other_row.end:
@@ -56,91 +72,109 @@ class CareerRow:
             return False
         return True
 
+    def render_args(self) -> dict[str, Any]:
+        return self.rich_colors() or dict(
+            color=Stripe.single_color(self.org)
+        )
+
+    @staticmethod
+    def single_color(org):
+        return Stripe.org_colors.get(org, Stripe.fallback_color)
+
+    @property
+    def all_orgs(self):
+        match re.match(r'^(\w+)([|/\\])(\w+)$', self.org):
+            case re.Match(group=group):
+                return set([group(1), group(3)])
+            case _:
+                return set([self.org])
+
+    def rich_colors(self):
+        match re.match(r'^(\w+)([|/\\])(\w+)$', self.org):
+            case re.Match(group=group):
+                return dict(
+                    facecolor=Stripe.single_color(group(1)),
+                    edgecolor=Stripe.single_color(group(3)),
+                    hatch=group(2),
+
+                )
+            case _:
+                return None
+
     def __repr__(self):
-        return f"({self.name}@{self.org},start={self.start},end={self.end},dur={self.duration})"
+        return f"({self.name}@{self.org},{self.start}..{self.end},dur={self.duration},layer={self.layer},band={self.band})"
+
+def layers(stripes: list[Stripe]):
+    # Group the list of stripes by layers, and yield them in-order
+    # Ordering is ASCII-lexical, i.e. A before B before C etc.
+    all_layers = sorted(set(stripe.layer for stripe in stripes))
+    for layer in all_layers:
+        yield (layer, (stripe for stripe in stripes if stripe.layer == layer))
+
+def log(message):
+    sys.stderr.write(message + "\n")
 
 def process(in_fd, out_fd):
     io = SkipComments(in_fd)
-    data = list(CareerRow(row) for row in csv.reader(io))
+    data = list(Stripe(row) for row in csv.reader(io))
 
-    # Data is formatted like this
-    # name,org,start-date,end-date
     # For each name, produce a list of line segments, each starting at start-date, ending at end-date
     # and with the color looked up by org in an org_colors map
-    colors = load_org_colors()
-    fallback_color = '#000'
 
     _fig, ax = plt.subplots(figsize=(10, 6),layout='constrained')
     loc = AutoDateLocator()
     loc.intervald[YEARLY] = [1]
     ax.xaxis.set_major_locator(loc)
     loc = AutoDateLocator()
-    loc.intervald[MONTHLY] = [3]
+    loc.intervald[MONTHLY] = [6]
     ax.xaxis.set_minor_locator(loc)
     ax.xaxis.grid(visible=True, alpha=0.4)
 
-
-    # Grab all intervals, sort by length descending
-    # Pick the first, longest one, place it on the first row. Remember the row for its org.
-    # Take the next one. If it overlaps in any items in the first row, put it on the next row and remember row.
-    # But if it doesn't overlap, add it to the first row and remember.
     labels = {}
-    grouped_by_name = groupby(data, lambda row: row.name)
+    orgs_used = set()
     rownum = 0
-    for name, rows in grouped_by_name:
-        sys.stderr.write(name + "\n")
-        labels[rownum] = name
-        # rows = sorted(list(rows), key=lambda row: row.duration, reverse=True)
-        rows = list(rows)
-        row_contents = defaultdict(list)
-        while rows:
-            longest = rows.pop(0)
-            which_row = None
-            # Find a row this can fit in
-            # Start with most packed row first, then go through less packed
-            for maybe_rownum, content in sorted(row_contents.items(), key=lambda rc: len(rc[1]), reverse=True):
-                if any(r.overlaps(longest) for r in content):
+    full_height = 0.8
+    for name, stripes in groupby(data, lambda row: row.name):
+        stripes = list(stripes)
+        # Rows for the same name are listed as consecutive groups
+        for layer_index, (ls, layer_stripes) in enumerate(layers(stripes)):
+            for stripe in layer_stripes:
+                if ls == '!':
+                    ax.vlines(stripe.start, 0, 1, transform=ax.get_xaxis_transform())
+                    ax.text(stripe.start, 0, stripe.name, rotation='vertical', transform=ax.get_xaxis_transform())
                     continue
-                else: # No overlap in this row
-                    which_row = maybe_rownum
-            if which_row is None:
-                # Didn't find a place
-                which_row = max(row_contents.keys()) + 1 if row_contents else rownum
-            row_contents[which_row].append(longest)
+                labels[rownum] = name
+                orgs_used |= stripe.all_orgs
+                match stripe.band:
+                    case (1, 1):
+                        stripe_height = full_height
+                        stripe_offset = 0
+                    case (n, d):
+                        stripe_height = full_height / d
+                        top = -full_height / 2 + stripe_height / 2
+                        stripe_offset = top + (n-1) * stripe_height
 
-        # At this point:
-        # rows has been consumed
-        # row_contents maps row numbers to lists of bars that fit there, longest first
-        # IDEA: Those aren't consecutive rows. Those are LAYERS
-        draw_rows = list(row_contents.items())
-        max_row = max(r for r, _bars in draw_rows)
-        frac_step = 1.0
+                ax.barh(rownum + layer_index - stripe_offset,
+                        stripe.duration,
+                        left=stripe.start,
+                        height=stripe_height,
+                        **stripe.render_args()
+                )
+        rownum += layer_index + 1
 
-        for rowindex, bars in draw_rows:
-            sys.stderr.write(f"{rowindex} {bars}\n")
-            offset = rowindex - rownum
-            for bar in bars:
-                ax.barh(rownum + offset * frac_step, bar.duration, left=bar.start, color=colors.get(bar.org, fallback_color), height=1.0)
-        rownum = rownum + math.ceil(frac_step * len(draw_rows)) + 1
+    # Create legend by adding patches
+    log(f"OU = {orgs_used}")
+    legend_artists = [pat.Rectangle((0, 0), 0.5, 0.5, color=Stripe.single_color(org)) for org in orgs_used]
+    legend_keys = list(orgs_used)
+    _fig.legend(legend_artists, legend_keys,loc='outside right lower')
 
     y_pos = list(labels.keys())
     ax.set_yticks(y_pos, [labels[i] for i in y_pos])
     ax.invert_yaxis()
 
-    # Create legend by adding patches
-    legend_artists = [pat.Rectangle((0, 0), 0.5, 0.5, color=color) for _key, color in colors.items()]
-    legend_keys = colors.keys()
-    _fig.legend(legend_artists, legend_keys,loc='outside right lower')
-
-
     ax.set_xlim(left=None, right=datetime.now())
-    # Important dates for The Greens
-    ax.vlines(ymd('2021-10-27'), 0, 1, transform=ax.get_xaxis_transform()) # Green Revolution
-    ax.vlines(ymd('2024-06-05'), 0, 1, transform=ax.get_xaxis_transform()) # Post-TBFTP Exits
     # plt.tight_layout()
     plt.savefig(out_fd, format='svg', pad_inches=0, transparent=True)
-
-
 
 
 def main():
