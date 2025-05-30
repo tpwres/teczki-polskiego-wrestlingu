a0b2118a45ccfa182296ba872121094b98d17518
Krzysztof Zych <k33rni@gmail.com>
2025-05-31 00:08:15 +0200

    Don't start zola. Instead print a .welcome message with instructions.

--- devcontainer-setup.sh
+++ devcontainer-setup.sh
@@ -24,5 +24,7 @@ npm install
 echo "Running make all with uv..."
 uv run make all plot
 
-echo "Starting Zola server on port 1111 in the background..."
-uv run zola serve --interface 0.0.0.0 --port 1111 &
+#echo "Starting Zola server on port 1111 in the background..."
+#uv run zola serve --interface 0.0.0.0 --port 1111 &
+
+
