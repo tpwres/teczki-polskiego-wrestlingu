93efb4eb9dd35be5d9fb1824370257257c4c6585
Krzysztof Zych <k33rni@gmail.com>
2025-12-17 23:28:50 +0100

    Misty reader: move row, col to class state

--- src/misty/reader.py
+++ src/misty/reader.py
@@ -6,68 +6,83 @@ from .liquid import LiquidExpr, LiquidContentBlock, DataBlock, LiquidSpan
 class Reader:
     def __init__(self, document):
         self.document = document
+        self.row: int = 1
+        self.col: int = 1
 
     def walk(self):
         yield from self.walk_nested(self.document)
 
-    def walk_nested(self, node, row=1, col=1):
+    def advance(self, *, rows=0, cols=0):
+        self.row += rows
+        self.col += cols
+
+    def cr(self):
+        self.col = 1
+
+    @property
+    def pos(self) -> (int, int):
+        return (self.row, self.col)
+
+    @property
+    def bol(self) -> (int, int):
+        return (self.row, 1)
+
+    def walk_nested(self, node, depth=1):
         for subnode in node.children:
             if hasattr(subnode, 'line_number'):
-                row = subnode.line_number
+                self.row = subnode.line_number
             nodename = type(subnode).__name__
 
-            # NOTE: putting row, col in ivars may help clean up this code
             match subnode:
                 case span_token.LineBreak():
-                    row += 1
-                    col = 1
+                    self.advance(rows=1)
+                    self.cr()
                 case span_token.Link(target=target, children=[span_token.RawText(content=content)]):
-                    yield nodename, (row, col), (target, content)
-                    col += len(target) + len(content) + 4
+                    yield nodename, self.pos, (target, content)
+                    self.advance(cols=len(target) + len(content) + 4)
                 case block_token.List():
-                    yield 'ListStart', (row, 1), ()
-                    for kls, (r, c), data in self.walk_nested(subnode, row, col):
-                        yield kls, (r, c), data
-                        row = r
-                    yield 'ListEnd', (row, 1), ()
+                    yield 'ListStart', self.bol, ()
+                    # for kls, (r, c), data in self.walk_nested(subnode):
+                    #     yield kls, (r, c), data
+                    yield from self.walk_nested(subnode, depth+1)
+                    yield 'ListEnd', self.bol, ()
                 case block_token.ListItem(children=children):
                     # prepend contains the additional list margin
-                    for kls, (r, c), data in self.walk_nested(subnode, row, col):
+                    for kls, (r, c), data in self.walk_nested(subnode, depth+1):
                         yield kls, (r, c + subnode.prepend), data
                 case Frontmatter(content=content):
-                    yield nodename, (row, 1), (Frontmatter.parse_frontmatter(content),)
+                    yield nodename, self.bol, (Frontmatter.parse_frontmatter(content),)
                 case DataBlock(head='card', content=card):
-                    yield 'Card', (row, 1), (card,)
+                    yield 'Card', self.bol, (card,)
                 case LiquidContentBlock(head=head):
-                    yield 'ContentBlockStart', (row, 1), (head,)
-                    yield from self.walk_nested(subnode)
-                    yield 'ContentBlockEnd', (row, 1), (head,)
-                # case LiquidContentBlock(head=head, content=content):
-                #     yield f'{head}', (row, 1), (content,)
+                    yield 'ContentBlockStart', self.bol, (head,)
+                    yield from self.walk_nested(subnode, depth+1)
+                    yield 'ContentBlockEnd', self.bol, (head,)
                 case LiquidExpr(content=content, head=head):
-                    yield f'={head}', (row, col), (content,)
-                    col += len(content) + 4
+                    yield f'={head}', self.pos, (content,)
+                    self.advance(cols=len(content) + 4)
                 case block_token.Heading(children=[span_token.RawText(content=raw)], level=level):
-                    yield 'Heading', (row, 1), (level, raw)
+                    yield 'Heading', self.bol, (level, raw)
+                    self.cr()
                 case block_token.Paragraph(): # Should be any block token with children?
-                    yield from self.walk_nested(subnode, row)
+                    yield from self.walk_nested(subnode, depth+1)
+                    self.cr()
                 # Must be above other generic span tokens
                 case span_token.RawText(content=content):
-                    yield 'Text', (row, col), (content,)
-                    col += len(content)
+                    yield 'Text', self.pos, (content,)
+                    self.advance(cols=len(content))
                 # For Emphasis, if needed, the type is in .delimiter (_|*|~)
                 case span_token.SpanToken(children=[span_token.RawText(content=content)]):
                     # Any unhandled inline node with just rawtext
-                    yield nodename, (row, col), (content,)
-                    col += len(content)
+                    yield nodename, self.pos, (content,)
+                    self.advance(cols=len(content))
                 case span_token.SpanToken(children=children):
-                    yield f'<{nodename}', (row, col), ()
-                    for kls, (r, c), data in self.walk_nested(subnode, row, col):
+                    yield f'<{nodename}', self.pos, ()
+                    for kls, (r, c), data in self.walk_nested(subnode, depth+1):
                         yield kls, (r, c), data
-                        row = r
-                    yield f'>{nodename}', (row, col), ()
+                        self.row = r
+                    yield f'>{nodename}', self.pos, ()
                 case _:
-                    breakpoint()
                     raise ValueError(f"Unhandled node {subnode}")
 
 
