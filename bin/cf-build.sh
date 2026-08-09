4ee10629f2c9f053878fff946aa856a6fb0983b2
Krzysztof Zych <k33rni@gmail.com>
2026-08-09 17:33:22 +0200

    log commands in seo

--- bin/cf-build.sh
+++ bin/cf-build.sh
@@ -27,6 +27,7 @@ create_config() {
 }
 
 setup_seo() {
+  set -x
   # Find the last-modified dates of all files under content/
   # Edit each file inline, inserting updated= into front matter
   git fetch --depth=50 origin $(git branch --show-current)
